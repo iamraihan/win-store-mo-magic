bd4b85eecf15d8bbd66e573686d2127e79818a1b
Raihan Ali <trueraihan@gmail.com>
2023-03-13 22:25:07 +0600

    catogry slider added

--- src/app/sections/Category/Category.js
+++ src/app/sections/Category/Category.js
@@ -1,28 +1,84 @@
 "use client";
 import "swiper/swiper-bundle.css";
 import styles from "./Category.module.css";
-import { EffectFade, Navigation, Pagination } from "swiper";
+import { Navigation } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
+import Image from "next/image";
+
 const Category = () => {
+  const dataList = [
+    {
+      id: 1,
+      title: "electronics",
+      imgURL: "/electronics.png",
+    },
+    {
+      id: 2,
+      title: "jewelery",
+      imgURL: "/fashion.png",
+    },
+    {
+      id: 3,
+      title: "men's clothing",
+      imgURL: "/appliances.png",
+    },
+    {
+      id: 4,
+      title: "women's clothing",
+      imgURL: "/babies-store.png",
+    },
+    {
+      id: 5,
+      title: "electronics",
+      imgURL: "/electronics.png",
+    },
+    {
+      id: 6,
+      title: "jewelery",
+      imgURL: "/fashion.png",
+    },
+    {
+      id: 7,
+      title: "men's clothing",
+      imgURL: "/appliances.png",
+    },
+    {
+      id: 8,
+      title: "women's clothing",
+      imgURL: "/babies-store.png",
+    },
+  ];
   return (
-    <div>
-      {" "}
-      <>
-        <Swiper
-          spaceBetween={30}
-          effect={"fade"}
-          navigation={true}
-          pagination={{
-            clickable: true,
-          }}
-          modules={[EffectFade, Navigation, Pagination]}
-          className="categorySlider"
-        >
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-          </SwiperSlide>
-        </Swiper>
-      </>
+    <div className={`${styles.categoryBg}`}>
+      <div className={`${styles.categoryContainer}`}>
+        <>
+          <Swiper
+            slidesPerView={4}
+            spaceBetween={33}
+            effect={"fade"}
+            navigation={true}
+            modules={[Navigation]}
+            className="categorySlider"
+          >
+            {dataList?.map((data) => (
+              <SwiperSlide key={data.id}>
+                <div className={`${styles.relative}`}>
+                  <Image
+                    src={`${data?.imgURL}`}
+                    width={272}
+                    height={199}
+                    alt={data?.title}
+                  />
+                  <div className={`${styles.shop}`}>
+                    <h4>{data?.title}</h4>
+                    <p className={`${styles.shopSubtitle}`}>Shop</p>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </>
+      </div>
     </div>
   );
 };
