2a7ea4f0d430a8a0e81ab7495f5bdd116ed1f666
Raihan Ali <trueraihan@gmail.com>
2023-03-13 15:36:47 +0600

    update

---
+++ src/app/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import styles from "./Navbar.module.css";
+const Navbar = () => {
+  return (
+    <div>
+      <h2 className={styles.navWrapper}>Navbar</h2>
+    </div>
+  );
+};
+
+export default Navbar;
