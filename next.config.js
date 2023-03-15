9a1e7de072ecff5a2e28f274f5cab0e9b8df09ab
Raihan Ali <trueraihan@gmail.com>
2023-03-16 01:55:23 +0600

    next config js changed

--- next.config.js
+++ next.config.js
@@ -6,12 +6,6 @@ const nextConfig = {
   images: {
     domains: ["fakestoreapi.com"],
   },
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
