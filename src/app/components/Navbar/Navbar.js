1b1813205c8829f110b3954fa7e29887024cabf3
Raihan Ali <trueraihan@gmail.com>
2023-03-16 14:40:48 +0600

    nav done

--- src/app/components/Navbar/Navbar.js
+++ src/app/components/Navbar/Navbar.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import styles from "./Navbar.module.css";
 const Navbar = () => {
   return (
@@ -11,23 +12,33 @@ const Navbar = () => {
               </div>
               <div className={styles.inputWrapper}>
                 <div>
-                  <select>
+                  <select className={styles.inputSelect}>
                     <option>All categories</option>
                     <option>option 2 </option>
                     <option>option 3 </option>
                   </select>
                 </div>
                 <div>
-                  <input type="text" placeholder="Search for products" />
-                  <img src="/search.png" alt="" />
+                  <input
+                    className={styles.inputText}
+                    type="text"
+                    placeholder="Search for products"
+                  />
+                </div>
+                <div>
+                  <img className={styles.submitBtn} src="/search.png" alt="" />
                 </div>
               </div>
             </div>
             <div className={styles.cartAreaWrapper}>
               <div>
                 <p className={styles.callNow}>Call Us Now</p>
-                <p className={styles.phone}>+011 5827918</p>
-                <p className={styles.phone}>Sign In</p>
+                <p className={styles.phone}>
+                  {" "}
+                  <img src="/headphones.svg" alt="" />
+                  +011 5827918
+                </p>
+                <p className={styles.signIn}>Sign In</p>
               </div>
               <div>
                 <img src="/user.png" alt="" />
@@ -37,12 +48,58 @@ const Navbar = () => {
               </div>
               <div className={styles.cartItem}>
                 <img src="/cart.png" alt="" />
-                <p>Cart</p>
+                <p className={styles.cartText}>Cart</p>
               </div>
             </div>
           </div>
         </div>
-        <div className={styles.bottomNav}></div>
+        <div className={styles.bottomNav}>
+          <div className={styles.bottomNavWrapper}>
+            <div className={styles.routesList}>
+              <div className={styles.categoryWrapper}>
+                <img src="/hamburger.svg" alt="" />
+                <p>Brouse By Category</p>
+              </div>
+              <Link className={styles.route} href="#">
+                <p>Home</p>
+              </Link>
+              <Link className={styles.route} href="#">
+                <p>Easy Monthly Installments</p>
+              </Link>
+              <Link className={styles.route} href="#">
+                <p>Easy Shop by Brands</p>
+              </Link>
+              <Link className={styles.route} href="#">
+                <p>Easy Become a Vendor</p>
+              </Link>
+            </div>
+            <div className={styles.socialIcons}>
+              <div className={styles.icon}>
+                <Link href="#">
+                  <img src="/fb.svg" alt="" />
+                </Link>
+              </div>
+              <div className={styles.icon}>
+                <Link href="#">
+                  {" "}
+                  <img src="/twitter.svg" alt="" />
+                </Link>
+              </div>
+              <div className={styles.icon}>
+                <Link href="#">
+                  {" "}
+                  <img src="/linkedIn.svg" alt="" />
+                </Link>
+              </div>
+              <div className={styles.icon}>
+                <Link href="#">
+                  {" "}
+                  <img src="/insta.svg" alt="" />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
